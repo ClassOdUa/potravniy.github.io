cae836d6ffc9ecbd6c9215d81d0e5bb062b3ccbf
Viacheslav <a05@ukr.net>
2015-11-18 19:28:49 +0200

    Auto fit updated for screen2

--- screen2.js
+++ screen2.js
@@ -1,13 +1,13 @@
 ﻿window.onload = function () {
     var screen1width = window.screen.width;
-    //  event
-    window.addEventListener("move", resizeWindow);
     //  Autofit
+    var intID = setInterval(resizeWindow, 200);
     function resizeWindow() {
         console.log("screen2 resize")
         if (window.screenLeft > screen1width) {
-            win.moveTo(window.screen.availLeft, 0);
+            win.moveTo(window.screen.availLeft, window.screen.availTop);
             win.resizeTo(window.screen.availWidth, window.screen.availHeight);
+            clearInterval(intID);
         }
     }
 }
