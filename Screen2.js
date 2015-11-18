d391693971599ba095dfa7d498c4a96f8bc3c3ee
Viacheslav <a05@ukr.net>
2015-11-18 17:37:18 +0200

    Added: screen2 mirroring and auto maximization.

---
+++ Screen2.js
@@ -0,0 +1,10 @@
+﻿window.onload = function () {
+    var screen1width = window.screen.width;
+    window.addEventListener("move", resizeWindow);
+    function resizeWindow() {
+        if (window.screenLeft > screen1width) {
+            win.moveTo(window.screenLeft, window.screenTop);
+            win.resizeTo(window.screen.width, window.screen.height);
+        }
+    }
+}
--- main.js
+++ main.js
@@ -173,7 +173,6 @@
     $screen2OpenCloseButton = $body.querySelector("button#screen2");
     $screen2OpenCloseButton.addEventListener('click', openCloseScreen2);
     function openCloseScreen2() {
-        console.log("openCloseScreen2", screen2 == null);
         if (screen2) screen2WindowClose();
         else screen2WindowCreate();
     }
@@ -181,11 +180,9 @@
         var strWindowFeatures = "menubar=no, location=no, locationbar=no, toolbar=no, personalbar=no, status=no, resizable=yes, scrollbars=no,status=no";
         var strWindowPositionAndSize = "height=500,width=400";
         screen2 = window.open("screen2.html", "screen2nd", strWindowPositionAndSize + "," + strWindowFeatures);
-//        console.log(screen2);
         screen2.onload = function () {
             $screen2Timer = screen2.document.querySelector("div#time_left");
             $screen2Message = screen2.document.querySelector("div#message_show");
-            console.log($screen2Timer, $screen2Message);
             $screen2OpenCloseButton.textContent = "Закрыть окно суфлера";
         };
     }
