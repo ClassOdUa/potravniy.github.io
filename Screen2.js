647ddacf64515a1b3f4d1c00142732f105e6fb69
Viacheslav <a05@ukr.net>
2015-11-18 17:51:55 +0200

    Misprint corrected

--- Screen2.js
+++ Screen2.js
@@ -1,6 +1,8 @@
 ﻿window.onload = function () {
     var screen1width = window.screen.width;
+
     window.addEventListener("move", resizeWindow);
+
     function resizeWindow() {
         if (window.screenLeft > screen1width) {
             win.moveTo(window.screenLeft, window.screenTop);
