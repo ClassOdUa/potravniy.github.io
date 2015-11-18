32de09cdac4f8033d9f93bfcbbc9187b1fe1776e
Viacheslav <a05@ukr.net>
2015-11-18 18:12:36 +0200

    Hope, changed.

---
+++ screen2.js
@@ -0,0 +1,12 @@
+﻿window.onload = function () {
+    var screen1width = window.screen.width;
+    //  event
+    window.addEventListener("move", resizeWindow);
+    //  Autofit
+    function resizeWindow() {
+        if (window.screenLeft > screen1width) {
+            win.moveTo(window.screenLeft, window.screenTop);
+            win.resizeTo(window.screen.width, window.screen.height);
+        }
+    }
+}
