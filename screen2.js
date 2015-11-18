be8fc018d6eac2eb72c3c34c71c15cd86865dda9
Viacheslav <a05@ukr.net>
2015-11-18 20:11:24 +0200

    Auto fit updated

--- screen2.js
+++ screen2.js
@@ -1,5 +1,6 @@
 ﻿window.onload = function () {
     var screen1width = window.screen.width;
+    var count = 0;
     //  Autofit
     var intID = setInterval(resizeWindow, 200);
     function resizeWindow() {
@@ -7,7 +8,8 @@
         if (window.screenLeft > screen1width) {
             window.moveTo(window.screen.availLeft, window.screen.availTop);
             window.resizeTo(window.screen.availWidth, window.screen.availHeight);
-            clearInterval(intID);
+            if(count > 20) clearInterval(intID);
+            count++;
         }
     }
 }
